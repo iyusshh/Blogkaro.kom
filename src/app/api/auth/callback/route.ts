bc168b5449961989c1b504fe87de316d27437c7d
iyusshh <aayushrai787@gmail.com>
2025-09-07 21:32:49 +0530

    fixed string type error in route.ts for setSession

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -34,7 +34,13 @@ export async function GET(req: Request) {
     create: { email: u.email, name: u.name, avatarUrl: u.picture }
   })
 
-  setSession({ userId: user.id, email: user.email, name: user.name, avatarUrl: user.avatarUrl || undefined })
+  setSession({
+  userId: user.id ?? "",
+  email: user.email ?? "",
+  name: user.name ?? "",
+  avatarUrl: user.avatarUrl ?? undefined
+});
+
 
   return Response.redirect(new URL('/', url).toString(), 302)
 }
